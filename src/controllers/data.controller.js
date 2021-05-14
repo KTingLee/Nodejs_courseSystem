0b25e59e90ada3f7366d9f464a5a4c93184e9851
JoeLee <joelee@bxb.tw>
2021-05-14 16:41:10 +0800

    refactory(選課畫面)：重構選課畫面 ＊本來是先「獲得各課程的狀態」，然後再「獲得課程清單」，兩個結合後建立出課程狀態的按鈕 ＊但實際上，獲得各課程狀態 api，就可以包含課程資訊與狀態，所以不用再呼叫 GET /api/courses（這隻 api 也只限定給 admin） ＊重構前端程式碼

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -7,7 +7,7 @@ const debug = require('debug')('app:ctrl:data')
 // 依照不同的使用者，各課程顯示的狀態會不一樣
 async function listCourseStatus(req, res, next) {
   const user = await UserDB.findOne({id: req.session.userId})
-  const courses = await CourseDB.find({})
+  let courses = await CourseDB.find({})
 
   // 紀錄學生所選課程的上課星期
   let days = []
@@ -18,33 +18,34 @@ async function listCourseStatus(req, res, next) {
   })
 
   // 再遍歷一次課程，紀錄各個課程對於當前學生的狀況
-  let coursesCondition = {}
-  courses.forEach(course => {
+  courses = courses.map(course => {
+    course = course.toObject()
     // 學生已經選過該課程
     if (user.courses.includes(course.id)) {
-      coursesCondition[course.id] = "已選課程";
+      course.status = '已選課程'
     }
     // 學生於該日已經選過課程(每日只能一門)
-    else if (days.indexOf(course.courseDay) != -1) {
-      coursesCondition[course.id] = "每日只限一門";
+    else if (days.includes(course.day)) {
+      course.status = '每日只限一門'
     }
     // 該課程已達上限
     else if (course.member <= 0) {
-      coursesCondition[course.id] = "課程已達上限";
+      course.status = '課程人數已達上限'
     }
     // 學生年級不符要求
-    else if (course.allow.indexOf(user.grade) == -1) {
-      coursesCondition[course.id] = "年級不符";
+    else if (!course.allow.includes(user.grade)) {
+      course.status = '年級不符'
     }
     // 學生所選課程已達上限(最多兩門課)
-    else if (days.length == 2) {
-      coursesCondition[course.id] = "已達選課上限";
+    else if (days.length === 2) {
+      course.status = '已達選課上限'
     }
     else {
-      coursesCondition[course.id] = "報名";
+      course.status = '報名'
     }
+    return course
   })
-  return res.send(coursesCondition)
+  return res.send(courses)
 }
 
 export default { listCourseStatus, }
